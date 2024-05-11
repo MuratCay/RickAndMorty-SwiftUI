d707d52829f1ab4587bfeff4305e74762ef702a0
Murat Cay <MuratTheBestOf@gmail.com>
2024-05-11 21:17:05 +0300

    screen added

--- RickAndMorty-SwiftUI/Features/Favourites/FavouritesScreen.swift
+++ RickAndMorty-SwiftUI/Features/Favourites/FavouritesScreen.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 struct FavouritesScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
 #Preview {
-    FavouritesScreen()
+    ContentView()
 }
--- RickAndMorty-SwiftUI/Features/Profile/ProfileScreen.swift
+++ RickAndMorty-SwiftUI/Features/Profile/ProfileScreen.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 struct ProfileScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Profile")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
 #Preview {
-    ProfileScreen()
+    ContentView()
 }
