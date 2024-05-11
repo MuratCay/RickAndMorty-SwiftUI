583d4b425aaa77aeabe012026d260608c999eb29
Murat Cay <MuratTheBestOf@gmail.com>
2024-05-11 21:16:09 +0300

    Screens Added

---
+++ RickAndMorty-SwiftUI/Features/Favourites/FavouritesScreen.swift
@@ -0,0 +1,18 @@
+//
+//  FavouritesScreen.swift
+//  RickAndMorty-SwiftUI
+//
+//  Created by Murat on 11.05.2024.
+//
+
+import SwiftUI
+
+struct FavouritesScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavouritesScreen()
+}
